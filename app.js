5de74006ac8ad8ec7f9264b36c52636e40e6b8d3
Nurullo <khuwaidullaev@gmail.com>
2021-01-09 20:42:16 +0500

    Using Passport.js to add Cookies and Sessions

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
 //jshint esversion:6
-
 //Variables
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-// const encrypt = require('mongoose-encryption');
-// const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const md5 = require('md5');
-const saltRounds = 10;
+const session = require('express-session');//1 Order is !important
+const passport = require('passport');//2
+const passportLocalMongoose = require('passport-local-mongoose');//3
+const { Passport } = require('passport');
+
 
 const app = express();
-
-console.log(process.env.SECRET);
-
 const port = 3000;
+console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: "Snyder's Cut is to be shown on HBO MAX",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); //manages our session
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true, 
     useUnifiedTopology:true
 });
 
+mongoose.set("useCreateIndex", true); // solves deprecation problem: collection.ensureIndex
 //Setting User Database
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:  ["password"] }); // !!!it should be added before you create Mongoose.model
-
-
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy()); //creates local Login strategy
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //GET connections
 app.get("/", function(req, res) {
@@ -52,42 +62,52 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
 // POST connections
 app.post("/register", function(req, res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-
-    const newUser = new User({
-        email:  req.body.username,
-        password: hash
-    });
-    newUser.save(function(err) {
-        if(err) {
-            console.log(err);
-        }else {
-            res.render("secrets"); //once registered successfully - "Jack Bauer is my hero" - pops up
-        }
-      });
-    });
+    User.register({username: req.body.username}, 
+        req.body.password, function(err, user) {
+            if(err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                });
+            }
+    }); 
+    
 });
 
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-                              
-    User.findOne({email: username}, function(err, foundUser) {
+   
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err) {
         if(err) {
             console.log(err);
         } else {
-            if(foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                 if(result === true) {
-                    res.render("secrets");
-                }
-             });
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
     });
+    
 });
 
 //Setting PORT
